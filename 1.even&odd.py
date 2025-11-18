fcb6fbdd9cf7377243cc0e752059d7539dc60cc8
Sanway_Das_VIT <sanway.25boe10009@vitbhopal.ac.in>
2025-11-18 22:28:26 +0530

    Add files via upload

---
+++ 1.even&odd.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a number:"))
+if a%2==0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+print("My Name Is Sanway Das.")
+print("My Student ID Is 25BOE10009")
